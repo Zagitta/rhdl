b6eb72cec9c8b4d4dfc0936efe17305d1e449d06
Simon Rasmussen <zetlon@gmail.com>
2017-09-15 12:31:20 +0200

    FromIter u8 to i64 impl

--- src/parser/utils.rs
+++ src/parser/utils.rs
@@ -3,11 +3,11 @@ use itertools::Itertools;
 use itertools::FoldWhile::{Continue, Done};
 use std::str::FromStr;
 
-pub trait FromIter<I, E> where I : Iterator, Self: Sized {
-    fn from_iter(iter: I) -> Result<Self, E>;
+pub trait FromIter<T, E> where Self: Sized {
+    fn from_iter<I: Iterator<Item=T>>(iter: I) -> Result<Self, E>;
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 pub enum ParseIntError {
     Empty, 
     InvalidDigit, 
@@ -16,107 +16,165 @@ pub enum ParseIntError {
 }
 
 
-impl<I: Iterator<Item=char>> FromIter<I, ParseIntError> for i64 {
-    fn from_iter(iter: I) -> Result<Self, ParseIntError> {
-        let mut i = iter.peekable();
-
-        let (positive, advance) = match i.peek() {
-            Some(c) => match *c {
-                '+' => (true, true),
-                '-' => (false, true),
-                _ => (true, false)
-            },
-            None => return Err(ParseIntError::Empty)
-        };
-
-        if advance {
-            let _ = i.next();
-        }
-
-        let mut res = 0i64;
-        if positive {
-            for c in i {
-               let x = match c.to_digit(10) {
-                    Some(d) => d as i64,
-                    None => return Err(ParseIntError::InvalidDigit) 
+macro_rules! impl_char_digit_parser {
+    ($T:ident) => {
+        impl FromIter<char, ParseIntError> for $T {
+            fn from_iter<I: Iterator<Item=char>>(mut iter: I) -> Result<Self, ParseIntError> {
+                let (positive, mut res) = match iter.next() {
+                    Some(c) => match c {
+                        '+' => (true, $T::default()),
+                        '-' => (false, $T::default()),
+                        _ => match c.to_digit(10) {
+                            Some(d) => (true, d as $T),
+                            None => return Err(ParseIntError::InvalidDigit)
+                        }
+                    },
+                    None => return Err(ParseIntError::Empty)
                 };
-                res = match res.checked_mul(10) {
-                    Some(res) => res,
-                    None => return Err(ParseIntError::Overflow)
-                };
-                res = match res.checked_add(x) {
-                    Some(res) => res,
-                    None => return Err(ParseIntError::Overflow)
-                };
+
+                if positive {
+                    for c in iter {
+                    let x = match c.to_digit(10) {
+                            Some(d) => d as $T,
+                            None => return Err(ParseIntError::InvalidDigit) 
+                        };
+                        res = match res.checked_mul(10) {
+                            Some(res) => res,
+                            None => return Err(ParseIntError::Overflow)
+                        };
+                        res = match res.checked_add(x) {
+                            Some(res) => res,
+                            None => return Err(ParseIntError::Overflow)
+                        };
+                    }
+                } else {
+                    for c in iter {
+                    let x = match c.to_digit(10) {
+                            Some(d) => d as $T,
+                            None => return Err(ParseIntError::InvalidDigit) 
+                        };
+                        res = match res.checked_mul(10) {
+                            Some(res) => res,
+                            None => return Err(ParseIntError::Underflow)
+                        };
+                        res = match res.checked_sub(x) {
+                            Some(res) => res,
+                            None => return Err(ParseIntError::Underflow)
+                        };
+                    }
+                }
+
+                Ok(res)
             }
-        } else {
-            for c in i {
-               let x = match c.to_digit(10) {
-                    Some(d) => d as i64,
-                    None => return Err(ParseIntError::InvalidDigit) 
-                };
-                res = match res.checked_mul(10) {
-                    Some(res) => res,
-                    None => return Err(ParseIntError::Underflow)
-                };
-                res = match res.checked_sub(x) {
-                    Some(res) => res,
-                    None => return Err(ParseIntError::Underflow)
-                };
-            }
-        }
-
-        Ok(res)
+        } 
     }
-} 
+}
+
+
+macro_rules! impl_u8_digit_parser {
+    ($T:ident) => {
+        impl FromIter<u8, ParseIntError> for $T {
+            fn from_iter<I: Iterator<Item=u8>>(mut iter: I) -> Result<Self, ParseIntError> {
+                let (positive, mut res) = match iter.next() {
+                    Some(c) => match c {
+                        b'+' => (true, $T::default()),
+                        b'-' => (false, $T::default()),
+                        _ => match (c as char).to_digit(10) {
+                            Some(d) => (true, d as $T),
+                            None => return Err(ParseIntError::InvalidDigit)
+                        }
+                    },
+                    None => return Err(ParseIntError::Empty)
+                };
+
+                if positive {
+                    for c in iter {
+                    let x = match (c as char).to_digit(10) {
+                            Some(d) => d as $T,
+                            None => return Err(ParseIntError::InvalidDigit) 
+                        };
+                        res = match res.checked_mul(10) {
+                            Some(res) => res,
+                            None => return Err(ParseIntError::Overflow)
+                        };
+                        res = match res.checked_add(x) {
+                            Some(res) => res,
+                            None => return Err(ParseIntError::Overflow)
+                        };
+                    }
+                } else {
+                    for c in iter {
+                    let x = match (c as char).to_digit(10) {
+                            Some(d) => d as $T,
+                            None => return Err(ParseIntError::InvalidDigit) 
+                        };
+                        res = match res.checked_mul(10) {
+                            Some(res) => res,
+                            None => return Err(ParseIntError::Underflow)
+                        };
+                        res = match res.checked_sub(x) {
+                            Some(res) => res,
+                            None => return Err(ParseIntError::Underflow)
+                        };
+                    }
+                }
+
+                Ok(res)
+            }
+        } 
+    }
+}
+
+impl_char_digit_parser!(i8);
+impl_char_digit_parser!(i16);
+impl_char_digit_parser!(i32);
+impl_char_digit_parser!(i64);
+impl_char_digit_parser!(u8);
+impl_char_digit_parser!(u16);
+impl_char_digit_parser!(u32);
+impl_char_digit_parser!(u64);
+
+
+impl_u8_digit_parser!(i8);
+impl_u8_digit_parser!(i16);
+impl_u8_digit_parser!(i32);
+impl_u8_digit_parser!(i64);
+impl_u8_digit_parser!(u8);
+impl_u8_digit_parser!(u16);
+impl_u8_digit_parser!(u32);
+impl_u8_digit_parser!(u64);
+
 
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use self::ParseIntError::*;
+
     #[test]
-    fn it_works() {
-        let tv = vec![
-            "123123",
-            "asda",
-            "980789",
-            "9789789678687634",
-            "12312331231253987192874998174",
-            "-123",
-            "123123asd",
-            "-a",
-            "---",
-            "+++",
-            "+123",
-            "+-a"
-        ];
+    fn fussy_parsing() {
+        
+    }
 
-        let EMPTY = i64::from_str("").unwrap_err();
-        let IDIGIT = i64::from_str("a").unwrap_err();
-        let OF = i64::from_str("12312331231253987192874998174").unwrap_err();
-        let UF = i64::from_str("-12312331231253987192874998174").unwrap_err();
+    #[test]
+    fn parsing_invalid_digit_fails() {
+        assert_eq!(i64::from_iter("asd".chars()), Err(InvalidDigit));
+        assert_eq!(i64::from_iter("123123asd".chars()), Err(InvalidDigit));
+        assert_eq!(i64::from_iter("+-a".chars()), Err(InvalidDigit));
+    }
 
-        for s in &tv {
-            let ri = i64::from_iter(s.chars());
-            let rs = i64::from_str(s);
-            
-            use self::ParseIntError::*;
-            
-            match (ri, rs) {
-                (Ok(lhs), Ok(rhs)) => assert_eq!(lhs, rhs),
-                (Err(lhs), Err(rhs)) => {
-                    assert!(match (lhs, rhs) {
-                        (Empty, EMPTY) => true,
-                        (InvalidDigit, IDIGIT) => true,
-                        (Overflow, OF) => true,
-                        (Underflow, UF) => true,
-                        _ => false
-                    });
-                },
-                _ => assert!(false)
-            }
-        }
+    #[test]
+    fn parsing_overflows() {
+        assert_eq!(i64::from_iter("12312331231253987192874998174".chars()), Err(Overflow));
+    }
+    #[test]
+    fn parsing_underflows() {
+        assert_eq!(i64::from_iter("-12312331231253987192874998174".chars()), Err(Underflow));
+    }
+    #[test]
+    fn parsing_empty() {
+        assert_eq!(i64::from_iter("".chars()), Err(Empty));
     }
 }
 
@@ -130,9 +188,16 @@ mod bench {
     const DATA: &'static str = "112321237451111111116123";
 
     #[bench]
-    fn bench_impl(b: &mut Bencher) {
+    fn bench_char_impl(b: &mut Bencher) {
         b.iter(|| {
             i64::from_iter(DATA.chars())
+        })
+    }
+
+    #[bench]
+    fn bench_u8_impl(b: &mut Bencher) {
+        b.iter(|| {
+            i64::from_iter(DATA.bytes())
         })
     }
 
